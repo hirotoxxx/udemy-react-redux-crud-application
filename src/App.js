3b01a548466a8335d4dfa693430d3e0cfc92e480
hirotoxxx <chloehellcatz@gmail.com>
2020-03-10 23:26:12 +0900

    [feat] use props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React from 'react';
 
-function App() {
-  const greeting = "Hello";
-  const dom = <h1>Hi!!{greeting}</h1>
-  return dom;
+const App = () => {
+  const profiles = [
+    { name: "Taro", age: 13 },
+    { name: "Hanako", age: 5 },
+    { name: "NoName" }
+  ]
+  return (
+    <div>
+      {
+        profiles.map((profile, index) => {
+          return <User name={profile.name} age={profile.age} key={index} />
+        })
+      }
+    </div>
+  )
+}
+
+const User = (props) => {
+  return <div>Hi, I am {props.name}, and {props.age} years old!</div>
+}
+
+User.defaultProps = {
+  age: 1
 }
 
 export default App;
