25b696cfabf172c944db7e3e0deac6856b508bb3
hirotoxxx <chloehellcatz@gmail.com>
2020-05-05 02:06:40 +0900

    [feat] implement event new component

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,6 +4,7 @@ import axios from 'axios'
 // typeの識別子であるincrement,decrementはreducerでも使うため、一箇所で文字列として定義し、再利用可能にしておく
 // reducerで活用するためexportする
 export const READ_EVENTS = 'READ_EVENTS'
+export const CREATE_EVENT = 'CREATE_EVENT'
 
 // イベントの取得・作成・更新・削除のURL
 const ROOT_URL = 'https://udemy-utils.herokuapp.com/api/v1'
@@ -17,4 +18,12 @@ const QUERYSTRING = '?token=token123'
 export const readEvents = () => async dispatch => {
   const response = await axios.get(`${ROOT_URL}/events${QUERYSTRING}`)
   dispatch({ type: READ_EVENTS, response })
+}
+
+// componentのevents_new.jsで取得したtitle,bodyをvaluesで受け取っている
+// axios.postで保存処理を行なっている。
+export const postEvent = values => async dispatch => {
+  console.log(values);
+  const response = await axios.post(`${ROOT_URL}/events${QUERYSTRING}`, values)
+  dispatch({ type: CREATE_EVENT, response })
 }
--- src/components/events_new.js
+++ src/components/events_new.js
@@ -1,19 +1,69 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
+// 入力フォームの作成に必用なcomponent
+import { Field, reduxForm } from 'redux-form'
 // ルーティングを行うパッケージreact-router-dom
-import { link } from 'react-router-dom'
-// import { postEvents } from '../actions'
+import { Link } from 'react-router-dom'
+import { postEvent } from '../actions'
 
 class EventsNew extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <div>newwwwww</div>
-      </React.Fragment>
-    )
-  }
+	constructor(props) {
+		super(props)
+		this.onSubmit = this.onSubmit.bind(this)
+	}
+	// renderFieldでinputタブの実装を行なっている
+	// meta のtouched, errorはredux-form特有のもの
+	// touchedは一度でもタッチされたら
+	// errorはvalidation errorの文言
+	renderField(field) {
+		const { input, label, type, meta: { touched, error } } = field
+
+		return (
+			<div>
+				<input {...input} placeholder={label} type={type} />
+				{/* touchedの瞬間にvalidationが動く */}
+				{touched && error && <span>{error}</span>}
+			</div>)
+	}
+
+	async onSubmit(values) {
+		// 外部のアクションで定義したアクションクリエイターを呼び出して、その応答を持って↓
+		await this.props.postEvent(values)
+		console.log(values);
+		// historyに履歴を追加している
+		this.props.history.push('/')
+	}
+
+	render() {
+		// renderが実行された時に１番に実行する
+		const { handleSubmit } = this.props
+		return (
+			<form onSubmit={handleSubmit(this.onSubmit)}>
+				<div><Field label='title' name="title" type="text" component={this.renderField} /></div>
+				<div><Field label='body' name="body" type="text" component={this.renderField} /></div>
+
+				<div>
+					<input type="submit" value="Submit" disabled={false} />
+					{/* 一覧画面に戻る */}
+					<Link to="/">Cancel</Link>
+				</div>
+			</form >
+		)
+	}
+
 }
+// validationを定義
+const validate = values => {
+	const errors = {}
+	// title,bodyがそれぞれ空だった場合、エラーを出す
+	if (!values.title) errors.title = 'Enter a title, please.'
+	if (!values.body) errors.body = 'Enter a body, please.'
 
-// const mapDispatchToProps = ({ postEvents })
+	// errorsに入れて返す関数
+	return errors
+}
+const mapDispatchToProps = ({ postEvent })
 
-export default connect(null, null)(EventsNew)
+export default connect(null, mapDispatchToProps)(
+	reduxForm({ validate, form: 'eventNewForm' })(EventsNew)
+)
